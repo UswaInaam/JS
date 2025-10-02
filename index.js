487fcdb871f29c318c6643ba5400c23b32edac08
uswa <uswainam267@gmail.com>
2025-10-02 00:58:59 -0700

    index file

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 
-let a=0;
-var b=5;
-const c=10;
+// let a=5;
+// let b=7;
+// let c= a*b;
+// let d=a/b;
+// let e=a+b;
+// let f=a-b;
 
-console.log("hello from js");
-console.log(a)
-document.getElementById("para").innerHTML=c;
 
-let btn=document.querySelectorAll("button");
+// const c=10;
 
-btn[0].addEventListener("click", () =>{
-document.body.style.backgroundColor= "red";
-});
-btn[1].addEventListener("click", () =>{
-document.body.style.backgroundColor= "green";
-});
- btn[2].addEventListener("click", () => {
-  document.body.style.backgroundColor="blue";
-});
-btn[3].addEventListener("click", () =>{
-    document.body.style.backgroundColor="purple";
-});
+// console.log("hello from js");
+// console.warn(a);
+// console.error(a);
+// console.info(a);
+// console.debug(a);
+// console.log(c);
+// console.log(a);
+// console.log(f);
+// console.log(b);
+// // console.table(a);
+
+// document.getElementById("para").innerHTML=f;
+// document.getElementById("paragraph").innerHTML=a;
+// document.getElementById("paragraph-1").innerHTML=b;
+// document.getElementById("paragraph-2").innerHTML=c;
+
+// let btn=document.querySelectorAll("button");
+
+// btn[0].addEventListener("click", () =>{
+// document.body.style.backgroundColor= "red";
+// });
+// btn[1].addEventListener("click", () =>{
+// document.body.style.backgroundColor= "green";
+// });
+//  btn[2].addEventListener("click", () => {
+//   document.body.style.backgroundColor="blue";
+// });
+// btn[3].addEventListener("click", () =>{
+//     document.body.style.backgroundColor="purple";
+// });
+
+
+console.log("hello World");
+console.log("hello World");
+console.log("hello World");
+console.log(2+2);
+console.log(3/3);
+console.log(5*5);
+
