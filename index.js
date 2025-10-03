35587cd61d9a5db752b04ad7acf280c0392c4414
uswa <uswainam267@gmail.com>
2025-10-02 23:05:00 -0700

    index file

--- index.js
+++ index.js
@@ -41,10 +41,93 @@
 // });
 
 
-console.log("hello World");
-console.log("hello World");
-console.log("hello World");
-console.log(2+2);
-console.log(3/3);
-console.log(5*5);
+// console.log("hello World");
+// console.log("hello World");
+// console.log("hello World");
+// console.log(2+2);
+// console.log(3/3);
+// console.log(5*5);
 
+
+// const a=5;
+// const b=7;
+//  if(a<b)
+//      console.log("a is  smaller then b")
+//     console.log("b is greater then a")
+// if(a>b){
+//      console.log("a is  smaller then b")
+// }
+// else{
+//     console.log("b is greater then a")
+// }
+// if(a==2){
+//     console.log("values are equal");
+
+// }
+
+const a = 110
+if (a > 100) {
+    console.log("a is greater then 100")
+}
+let b = "admin"
+
+if (b == "admin") {
+    console.log("Welcome")
+}
+let c = 25
+if (c%25==0) {
+console.log("a is divisible by 5")
+}
+
+const d=2
+if (d%2==0) {
+   console.log("number is even") 
+} else {
+    console.log("number is odd")
+}
+
+let age=20
+if (age>=18) {
+    console.log("adult")
+} else {
+    console.log("minor")
+}
+ let temperature=32
+ if (temperature>30) {
+    console.log("hot")
+ } else {
+    console.log("cool")
+ }
+ let password=1234
+ if (password=1234) {
+    console.log("Access granted")
+ } else {
+    console.log("Access Denied")
+ }
+
+ let day="monday"
+ if (day=="sunday") {
+    console.log("Holiday")
+ } else {
+    console.log("working day")
+ }
+
+ let num1=15
+ let num2=20
+ if (num1>=num2) {
+    console.log("First is larger")
+ } else {
+    console.log("second is larger or equal")
+ }
+ let isLoggedIn=false
+ if (isLoggedIn) {
+    console.log("Dashboard")
+ } else {
+    console.log("Login page")
+ }
+ let fruit="banana"
+ if (fruit=="apple") {
+    console.log("red")
+ } else {
+    console.log("not apple")
+ }
