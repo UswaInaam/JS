3e4094c3c81716f2035d98b90e0bebca3e27164f
uswa <uswainam267@gmail.com>
2025-10-08 08:20:44 -0700

    js file

--- index.js
+++ index.js
@@ -65,69 +65,378 @@
 
 // }
 
-const a = 110
-if (a > 100) {
-    console.log("a is greater then 100")
-}
-let b = "admin"
+// const a = 110
+// if (a > 100) {
+//    console.log("a is greater then 100")
+// }
+// let b = "admin"
 
-if (b == "admin") {
-    console.log("Welcome")
-}
-let c = 25
-if (c%25==0) {
-console.log("a is divisible by 5")
-}
+// if (b == "admin") {
+//    console.log("Welcome")
+// }
+// let c = 25
+// if (c % 25 == 0) {
+//    console.log("a is divisible by 5")
+// }
 
-const d=2
-if (d%2==0) {
-   console.log("number is even") 
-} else {
-    console.log("number is odd")
-}
+// const d = 2
+// if (d % 2 == 0) {
+//    console.log("number is even")
+// } else {
+//    console.log("number is odd")
+// }
 
-let age=20
-if (age>=18) {
-    console.log("adult")
-} else {
-    console.log("minor")
-}
- let temperature=32
- if (temperature>30) {
-    console.log("hot")
- } else {
-    console.log("cool")
- }
- let password=1234
- if (password=1234) {
-    console.log("Access granted")
- } else {
-    console.log("Access Denied")
- }
+// let age = 20
+// if (age >= 18) {
+//    console.log("adult")
+// } else {
+//    console.log("minor")
+// }
+// let temperature = 32
+// if (temperature > 30) {
+//    console.log("hot")
+// } else {
+//    console.log("cool")
+// }
+// let password = 1234
+// if (password = 1234) {
+//    console.log("Access granted")
+// } else {
+//    console.log("Access Denied")
+// }
 
- let day="monday"
- if (day=="sunday") {
-    console.log("Holiday")
- } else {
-    console.log("working day")
- }
+// let day = "monday"
+// if (day == "sunday") {
+//    console.log("Holiday")
+// } else {
+//    console.log("working day")
 
- let num1=15
- let num2=20
- if (num1>=num2) {
-    console.log("First is larger")
- } else {
-    console.log("second is larger or equal")
- }
- let isLoggedIn=false
- if (isLoggedIn) {
-    console.log("Dashboard")
- } else {
-    console.log("Login page")
- }
- let fruit="banana"
- if (fruit=="apple") {
-    console.log("red")
- } else {
-    console.log("not apple")
- }
+// }
+
+// let num1 = 15
+// let num2 = 20
+// if (num1 >= num2) {
+//    console.log("First is larger")
+// } else {
+//    console.log("second is larger or equal")
+// }
+// let isLoggedIn = false
+// if (isLoggedIn) {
+//    console.log("Dashboard")
+// } else {
+//    console.log("Login page")
+// }
+// let fruit = "banana"
+// if (fruit == "apple") {
+//    console.log("red")
+// } else {
+//    console.log("not apple")
+// }
+// let e = 0
+// if (e > 5) {
+//    console.log("e is less then 5")
+// } else if (e < 5) {
+//    console.log("e is greatet then 5")
+// }
+// else {
+//    console.log(" e is equal to 0")
+// }
+// //terray
+// let Age = 23;
+// let text = (Age < 18) ? "Minor" : "adult";
+
+// (Age < 18) ? console.log("Minor") : console.log("Adult")
+// console.log(text);
+
+// //for loop
+
+// for (let i = 0; i > 5; i++) {
+//    console.log("Uswa")
+// }
+// //while loop
+
+// let i = 0;
+// while (i < 10) {
+//    console.log(i);
+//    i++;
+// }
+
+// //string
+// let intro = "My name is 'uswa'";
+// let intro1 = 'My name is "uswa"';
+// console.log(intro);
+// console.log(intro1);
+
+// //template string
+
+// let car = `Car is a "special" vehicle`;
+// console.log(car);
+
+// const seats = 4;
+// let car1 = `Car is a ${seats} vehicle`;
+// console.log(car1);
+
+// let course = "This is Web Application Development"
+// console.log(course.length);
+// console.log(course.toLowerCase(course));
+// console.log(course.toUpperCase(course));
+
+// let id = 2047;
+// console.log(id.toString());
+
+
+// let earth = `
+// Our earth is safe
+// and each galaxy have its own stars`;
+// console.log(earth);
+
+// //string methods
+
+// console.log(earth.charAt(3));
+// console.log(earth.charCodeAt(1));
+// console.log(earth.codePointAt(1));
+
+// let emoji = "😁"
+// console.log(emoji.charCodeAt(1));
+
+// console.log(earth.at(2));
+
+// const first_name = "Uswa";
+// const last_name = "Inaam";
+// const full_name = first_name.concat(` `, last_name);
+// const name = first_name.concat(last_name);
+
+// console.log(full_name);
+// console.log(name);
+
+//  let text1 = "hello World! \uD800";
+//  console.log(text1.isWellFormed());
+
+// console.log(text1.toWellFormed());
+
+//   let text2="Apple, Banana, Kiwi";
+//   let part = text2.slice(0, 5);
+//  let text2 = "Apple, Banana, Kiwi";
+//  let part = text2.slice(-12);
+// let text2 = "Apple, Banana, Kiwi";
+// let part = text2.slice(-12, -6);
+// console.log(part);
+
+
+
+
+
+
+
+// let name3="Uswa";
+// let name4="inaam❤";
+// let name5=name3.concat(' ',name4);
+// console.log(name5.toLocaleUpperCase());
+
+// let text = "i love Pakistan";
+// let part = text.slice(-10);
+// console.log(text.length);
+// console.log(part);
+// let text1 = "        hello World!       "
+// let text2 = text1.trimEnd();
+// console.log(text2);
+
+
+// let Access = text[14];
+// console.log(Access);
+
+
+// let text3 = text.padStart(27, "hello ");
+// console.log(text3);
+
+// var repeat = text.repeat(2);
+// console.log(repeat);
+// // (replace & replaceAll is a case sensitive)
+// var text_string = "Welcome to ChiNa, China is a frnd of pakistan";
+// text_string = text_string.replaceAll("ChiNa", "Pakistan");
+// console.log(text_string);
+
+
+
+
+
+// var names = "❤areeba,uswa,saffa❤"
+// names = names.split(",");
+// console.log(names[1]);
+
+// let number = 5e2;
+// console.log(number);
+
+// let a = 0.1;
+// let b = 0.2;
+// console.log(a + b);
+
+// console.log(typeof b);
+
+
+// let num=2.56778;
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(2));
+
+// let d=5;
+// let e="apple";
+// let f=d/e;
+
+
+// console.log(f);
+// if(isNaN(f))
+// {
+// console.log("the result is Nan");
+// }
+
+// var intro=" 10.5 years old";
+// console.log(parseFloat(intro));
+
+// names = "Uswa";
+// console.log(names);
+// var names = "Areeba";
+// let age = 30;
+// age = 25;
+// console.log(names, age);
+// number = 1;
+// number = 2;
+// number = 3;
+// //  const a=56;
+// a = 60;
+// console.log(a);
+
+// function hello(){
+//     console.log("hello");
+// }
+// hello();
+// function sum(a, b) {
+//     console.log(a + b);
+// } sum(2, 2)
+
+// function sumall(a, b, c, d, e, f, g, h) {
+//     console.log(a + b + c + d + e + f + g + h)
+// }
+
+// sumall(1, 2, 3, 4, 5, 6, 7, 8)
+// function sum(a, b) {
+//     return (a + b)
+// }
+// let a = sum(4, 6)
+// console.log(a);
+
+// let f = function () {
+//     return "hello";
+// }
+// console.log(f());
+
+
+
+
+//arrow function
+
+const double=(num)=>num * 2;
+let input=prompt("enter a number to double:");
+let number=parseInt(input);
+let result=double(number);
+console.log(`the double of ${number} is ${result}`);
+// let intro = () => {
+//     console.log("Uswa");
+// }
+// intro();
+
+// let intro1 = (name) => {
+//     console.log(`hello my name is ${name}`);
+// }
+// intro1("uswa");
+
+// function argument() {
+//     return ("hey! how r you")
+// }
+// console.log(argument());
+
+// function sum(y, z) {
+//     console.log(y + z);
+// }
+// sum(6, 7);
+
+// function sum2(y, z) {
+//     return (y + z)
+// }
+// let x = sum2(4, 5);
+// console.log(x);
+
+
+// function number(a) {
+//     if (a % 2 == 0) {
+//         console.log("number is even");
+//     }
+//     else {
+//         console.log("number is odd")
+//     }
+
+// }
+// number(4);
+
+// function square(b) {
+//     console.log(b * b)
+// }
+// square(5);
+
+// function convert(celsius) {
+
+//     return ((celsius * 9 / 5) + 32)
+// }
+// let c = convert(37)
+// console.log(c);
+
+// //regular expression (i,r =>flags)
+// // let text="there are many teachers in my school but aslam is my favourite teacher"
+
+// // let n=text.search(/my/g);
+// // console.log(n);
+
+// // let text="there are many teachers in my school but aslam is my favourite teacher"
+// // let pattern=/my/g
+// // let result=text.match(pattern);
+// // console.log(result);
+
+
+// const phoneNum=/^((\+92)?(0092)?(92)?(0)?)(3)([0-9]{9})$/gm;
+// let phone="34201-1214092-9"
+// let phone_result=phoneNum.test(phone);
+// console.log(phone_result)
+
+// const cnicRegx=/^[0-9]{5}-[0-9]{7}-[0-9]$/;
+// let cnic="34201-1214092-9"
+// let cnic_result=cnicRegx.test(cnic);
+// console.log(cnic_result);
+
+// // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// const emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/g;
+// let email="uswa@gmail.com"
+// let email_result=emailRegex.test(email);
+// console.log(email_result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
